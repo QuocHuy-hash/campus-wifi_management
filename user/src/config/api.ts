8f7de94e3339252d1b438f1d0818fb3df0a7ff87
QuocHuy <102716105+QuocHuy-hash@users.noreply.github.com>
2026-04-06 14:02:21 +0700

    cấu hình lại login auto gán role

--- user/src/config/api.ts
+++ user/src/config/api.ts
@@ -3,4 +3,4 @@
  * Centralized configuration for API endpoints
  */
 
-export const API_BASE_URL = 'https://57dc-14-191-221-9.ngrok-free.app/api/v1';
+export const API_BASE_URL = 'https://186c-14-191-91-150.ngrok-free.app/api/v1';
